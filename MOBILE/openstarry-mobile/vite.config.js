8a04de9ec7cde1600ab35b791bb425738f758833
tomysh <tomysh1337@users.noreply.github.com>
2026-09-19 13:57:30 +0800

    fix: exclude Android build output from Vite file watcher

--- MOBILE/openstarry-mobile/vite.config.js
+++ MOBILE/openstarry-mobile/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 export default defineConfig({
-  server: { fs: { allow: ['.', '../../SHARED/workbench'] } }, build: { assetsInlineLimit: 0 },
+  server: { fs: { allow: ['.', '../../SHARED/workbench'] }, watch: { ignored: ['**/android/**'] } }, build: { assetsInlineLimit: 0 },
   plugins: [{ name: 'offline-asset-manifest', generateBundle(_options, bundle) {
     this.emitFile({ type: 'asset', fileName: 'asset-manifest.json', source: JSON.stringify(Object.keys(bundle).filter(name => name.startsWith('assets/')).map(name => '/' + name)) })
   } }]
